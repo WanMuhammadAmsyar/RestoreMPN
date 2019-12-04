1be9a77d7130e667a380209271700603d327c480
WanMuhammadAmsyar <one.amsyar@gmail.com>
2019-12-04 17:13:21 +0800

    Edited to handle error cause by parsing text to int

--- RestoreMPN/Program.cs
+++ RestoreMPN/Program.cs
@@ -7,6 +7,8 @@ using System.IO;
 using Excel = Microsoft.Office.Interop.Excel;
 using System.Runtime.InteropServices;
 using MySql.Data.MySqlClient;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace RestoreMPN
 {
@@ -34,7 +36,7 @@ namespace RestoreMPN
             string[] listFiles;
 
             Console.WriteLine("Please Insert the Data Folder Path");
-            folderName = Console.ReadLine();
+            folderName = "C:\\Users\\User\\Desktop\\TimeLogger";
             Console.Clear();
             listFiles = Directory.GetFiles(folderName);
             readExcelData(listFiles);
@@ -53,61 +55,66 @@ namespace RestoreMPN
 
                 Random random = new Random();
 
-                try
+                conn.Open();
+                Excel.Application excel = new Excel.Application();
+                Excel.Workbook excelbook = excel.Workbooks.Open(s);
+                Excel._Worksheet excelsheet = excelbook.Sheets[1];
+                Excel.Range range = excelsheet.UsedRange;
+                for (int i = 2; i < range.Rows.Count; i++)
                 {
-                    conn.Open();
-                    Excel.Application excel = new Excel.Application();
-                    Excel.Workbook excelbook = excel.Workbooks.Open(s);
-                    Excel._Worksheet excelsheet = excelbook.Sheets[1];
-                    Excel.Range range = excelsheet.UsedRange;
-                    for (int i = 2; i < range.Rows.Count; i++)
-                    {
-                        deviceUserId = Convert.ToString(range.Cells[i, 2].Value2);
-                        tarikhIn = range.Cells[i, 3].Text;
-                        tarikhOut = range.Cells[i, 4].Text;
-                        timeIn = range.Cells[i, 5].Text;
-                        timeOut = range.Cells[i, 6].Text;
+                    deviceUserId = Convert.ToString(range.Cells[i, 2].Value2);
+                    tarikhIn = range.Cells[i, 3].Text;
+                    tarikhOut = range.Cells[i, 4].Text;
+                    timeIn = range.Cells[i, 5].Text;
+                    timeOut = range.Cells[i, 6].Text;
 
-                        UID = deviceUserId.ToCharArray();
-                        UID[0] = '3';
-                        updatedstring = new string(UID);
-                        if (timeIn != "NULL")
+                    UID = deviceUserId.ToCharArray();
+                    UID[0] = '3';
+                    updatedstring = new string(UID);
+                    if (timeIn != "NULL")
+                    {
+                        Console.WriteLine(tarikhIn + " " + timeIn);
+                        commandIn = "INSERT INTO `raw`(`StaffID`, `TID`, `TimeIN`, `TimeID`, `LogType`, `FlagProses`, `EnrollID`, `FlagUpdate`) VALUES (0,'" + random.Next(6, 12).ToString() + "','" + tarikhIn + " " + timeIn + "',0,0,0," + updatedstring + ",0)";
+                        MySqlCommand command = new MySqlCommand(commandIn, conn);
+                        try
                         {
-                            Console.WriteLine(tarikhIn + " " + timeIn);
-                            commandIn = "INSERT INTO `raw`(`StaffID`, `TID`, `TimeIN`, `TimeID`, `LogType`, `FlagProses`, `EnrollID`, `FlagUpdate`) VALUES (0,'" + random.Next(6, 12).ToString() + "','" + tarikhIn + " " + timeIn + "',0,0,0," + updatedstring + ",0)";
-                            MySqlCommand command = new MySqlCommand(commandIn, conn);
                             command.ExecuteNonQuery();
                         }
-                        if (timeOut != "NULL")
+                        catch
                         {
-                            Console.WriteLine(tarikhOut + " " + timeOut);
-                            commandOut = "INSERT INTO `raw`(`StaffID`, `TID`, `TimeIN`, `TimeID`, `LogType`, `FlagProses`, `EnrollID`, `FlagUpdate`) VALUES ('0','" + random.Next(6, 12).ToString() + "','" + tarikhOut + " " + timeOut + "','0','0','false','" + updatedstring + "','false')";
-                            MySqlCommand command = new MySqlCommand(commandOut, conn);
-                            command.ExecuteNonQuery();
+                            failed++;
                         }
                     }
-                    GC.Collect();
-                    GC.WaitForPendingFinalizers();
-                    Marshal.ReleaseComObject(range);
-                    Marshal.ReleaseComObject(excelsheet);
+                    if (timeOut != "NULL")
+                    {
+                        Console.WriteLine(tarikhOut + " " + timeOut);
+                        commandOut = "INSERT INTO `raw`(`StaffID`, `TID`, `TimeIN`, `TimeID`, `LogType`, `FlagProses`, `EnrollID`, `FlagUpdate`) VALUES ('0','" + random.Next(6, 12).ToString() + "','" + tarikhOut + " " + timeOut + "','0','0','false','" + updatedstring + "','false')";
+                        MySqlCommand command = new MySqlCommand(commandOut, conn);
+                        try
+                        {
+                            command.ExecuteNonQuery();
+                        }
+                        catch
+                        {
+                            failed++;
+                        }
 
-                    //close and release
-                    excelbook.Close();
-                    Marshal.ReleaseComObject(excelbook);
+                    }
+                }
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
+                Marshal.ReleaseComObject(range);
+                Marshal.ReleaseComObject(excelsheet);
 
-                    //quit and release
-                    excel.Quit();
-                    Marshal.ReleaseComObject(excel);
-                }
-                catch
-                {
-                    failed++;
-                    continue;
-                }
-                finally
-                {
-                    conn.Close();
-                }
+                //close and release
+                excelbook.Close();
+                Marshal.ReleaseComObject(excelbook);
+
+                //quit and release
+                excel.Quit();
+                Marshal.ReleaseComObject(excel);
+
+                conn.Close();
             }
         }
 
